b15b1a1e81822222bfd712570f00955701a38ab0
perassi <horacio2jp@gmail.com>
2023-01-31 09:50:27 -0800

    Added label "Saved as a link"

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ProductContext } from '../../../contexts/productContext'
 import { ReturnButton } from '../../common/ReturnButton/ReturnButton'
 
 import { ChestrIcon } from '../../icons/ChestrIcon'
@@ -6,12 +7,20 @@ import { ChestrIcon } from '../../icons/ChestrIcon'
 import './Header.scss'
 
 export const Header = () => {
+  const { pageParsedData } = useContext(ProductContext)
+
   return (
     <div className="header">
       <div className="header-content">
         <ChestrIcon />
 
-        <p className="header-title text-color-primary-600">Saved!</p>
+        {pageParsedData?.product?.image ? (
+          <p className="header-title text-color-primary-600">Saved!</p>
+        ) : (
+          <p className="header-title">
+            Saved as <span className="text-color-primary-600">link</span> 🔗
+          </p>
+        )}
       </div>
 
       <ReturnButton />
