1ad2ca2d5f42ce021e4cefbb5fb68e5779c89cdb
perassi <horacio2jp@gmail.com>
2023-01-31 17:48:33 +0100

    + iframe popup

--- src/pages/popup/App.tsx
+++ src/pages/popup/App.tsx
@@ -1,51 +1,41 @@
 import React from 'react'
-import { storage } from '@extend-chrome/storage'
-import { initializeApp } from 'firebase/app'
-import {
-  // connectAuthEmulator,
-  getAuth,
-  signInWithCustomToken,
-} from 'firebase/auth'
-import { firebaseConfig } from '../../firebaseConfig'
-
-initializeApp(firebaseConfig)
-// connectAuthEmulator(getAuth(), 'http://localhost:9099', {
-//   disableWarnings: true,
-// })
+import { WEB_URL } from '../../config'
 
 const App = (): JSX.Element => {
-  const [data, setData] = React.useState(undefined)
-  const [token, setToken] = React.useState<any>(undefined)
-  const [userCredential, setUserCredential] = React.useState<any>(undefined)
+  const iframeRef = React.useRef<HTMLIFrameElement>()
   React.useEffect(() => {
-    storage.local.get('token').then(({ token }) => {
-      setToken(token)
-      signInWithCustomToken(getAuth(), token).then((userCredential) => {
-        setUserCredential(userCredential)
+    if (iframeRef.current) {
+      window.addEventListener('message', function (e) {
+        if (e.data == 'ready')
+          chrome.tabs
+            .query({ active: true, currentWindow: true })
+            .then(([tab]) =>
+              chrome.scripting.executeScript(
+                {
+                  target: {
+                    tabId: tab.id!,
+                  },
+                  func: () => window.__CHESTR__,
+                },
+                ([{ result }]: any) => {
+                  iframeRef.current?.contentWindow?.postMessage(result, '*')
+                },
+              ),
+            )
+        else if (e.data == 'login') {
+          chrome.tabs.create({ url: WEB_URL + '/login' })
+        }
       })
-    })
-    chrome.tabs.query({ active: true, currentWindow: true }).then(([tab]) =>
-      chrome.scripting.executeScript(
-        {
-          target: {
-            tabId: tab.id!,
-          },
-          func: () => window.__CHESTR__,
-        },
-        ([{ result }]: any) => {
-          console.log('result', result)
-          return setData(result)
-        },
-      ),
-    )
-  }, [])
+    }
+  }, [iframeRef.current])
   return (
     <div>
-      <h1>Popup Page</h1>
-      <p>If you are seeing this, React is working!</p>
-      <pre>{JSON.stringify(token, null, 2)}</pre>
-      <pre>{JSON.stringify(userCredential, null, 2)}</pre>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <iframe
+        ref={iframeRef as React.LegacyRef<HTMLIFrameElement>}
+        src={WEB_URL + '/extension/popup'}
+        width={460}
+        height={(460 / 9) * 16}
+      ></iframe>
     </div>
   )
 }
--- src/serviceWorker/index.ts
+++ src/serviceWorker/index.ts
@@ -1,57 +1,12 @@
-import { storage } from '@extend-chrome/storage'
-// import preset from "@chestrapp/presets";
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { initializeAuth } from "firebase/auth";
-// import { initializeAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBgKQmVVt4OBkIILBN_MFIjFvFRBNwMquU",
-//     authDomain: "chestrapp.firebaseapp.com",
-//     databaseURL: "https://chestrapp-default-rtdb.firebaseio.com",
-//     projectId: "chestrapp",
-//     storageBucket: "chestrapp.appspot.com",
-//     messagingSenderId: "496271595233",
-//     appId: "1:496271595233:web:7b663daf53c9203b6909a0",
-//     measurementId: "G-VZ1J6363K0"
-// };
-
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const auth = initializeAuth(app);
-// const analytics = initializeAnalytics(app, {
-//     config: {
-//         send_page_view: false,
-//     },
-
-// });
+import { WEB_URL } from '../config'
 
 console.log('ServiceWorker script')
-
-// chrome.action.onClicked.addListener(
-//     (tab: chrome.tabs.Tab) => {
-//         chrome.scripting.executeScript({
-//             target: {
-//                 tabId: tab.id!
-//             },
-//             func: () => window.__CHESTR__
-//         },
-//             ([{ result }]: any) => {
-//                 console.log(result)
-//                 // chrome.action.setPopup({})
-//             }
-//         )
-//     }
-// )
 
 chrome.action.disable()
 
 chrome.tabs.onUpdated.addListener(
     (tabId: number, changeInfo: chrome.tabs.TabChangeInfo, tab: chrome.tabs.Tab) => {
+        console.log('chrome.tabs.onUpdated', changeInfo.status)
         if (changeInfo.status === 'complete') {
             chrome.scripting.executeScript({
                 target: {
@@ -75,21 +30,15 @@ chrome.tabs.onUpdated.addListener(
                             })
                         }
                     }
-                ).catch(() => undefined)
+                ).catch(console.error)
         }
     }
 )
 
-chrome.runtime.onMessageExternal.addListener(
-    (request, sender, sendResponse) => {
-        storage.local.set(request)
-    }
-);
-
 chrome.runtime.onInstalled.addListener(details => {
     if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
         chrome.tabs.create(
-            { url: "https://chestr.com/sign-up" },
+            { url: WEB_URL + "/sign-up" },
         )
     }
 });
