8ecf2e71b61e00466b21a07a929845dedff01b59
perassi <horacio2jp@gmail.com>
2023-02-06 08:23:39 -0800

    Iframe integration started

--- src/pages/popup/App.tsx
+++ src/pages/popup/App.tsx
@@ -1,21 +1,54 @@
-import React from 'react'
-
-import { Layout } from '../../components/layout/layout'
-import { FirebaseContextProvider } from '../../contexts/firebaseContext'
-import { ProductContextProvider } from '../../contexts/productContext'
+import React, { useEffect, useRef } from 'react'
+import { WEB_URL } from '../../config'
 
 import './App.css'
 
 const App = (): JSX.Element => {
+  const iframeRef = useRef<HTMLIFrameElement>()
+
+  useEffect(() => {
+    if (iframeRef.current) {
+      window.addEventListener('message', function (e) {
+        if (e.data == 'ready')
+          chrome.tabs
+            .query({ active: true, currentWindow: true })
+            .then(([tab]) =>
+              chrome.scripting.executeScript(
+                {
+                  target: {
+                    tabId: tab.id!,
+                  },
+                  func: () => window.__CHESTR__,
+                },
+                ([{ result }]: any) => {
+                  iframeRef.current?.contentWindow?.postMessage(
+                    result,
+                    WEB_URL + '/extension/popup',
+                  )
+                },
+              ),
+            )
+        else if (e.data == 'login') {
+          chrome.tabs.create({ url: WEB_URL + '/login' })
+        }
+      })
+    }
+  }, [iframeRef.current])
+
   return (
-    <ProductContextProvider>
-      <FirebaseContextProvider>
-        <Layout />
-        {/* <pre>{JSON.stringify(token, null, 2)}</pre>
-      <pre>{JSON.stringify(userCredential, null, 2)}</pre>
-      <pre>{JSON.stringify(data, null, 2)}</pre> */}
-      </FirebaseContextProvider>
-    </ProductContextProvider>
+    // <ProductContextProvider>
+    //   <FirebaseContextProvider>
+    //     <Layout />
+    //   </FirebaseContextProvider>
+    // </ProductContextProvider>
+    <div>
+      <iframe
+        ref={iframeRef as React.LegacyRef<HTMLIFrameElement>}
+        src={WEB_URL + '/extension/popup'}
+        width={460}
+        height={(460 / 9) * 16}
+      ></iframe>
+    </div>
   )
 }
 
--- src/serviceWorker/index.ts
+++ src/serviceWorker/index.ts
@@ -1,52 +1,6 @@
-import { storage } from '@extend-chrome/storage'
-// import preset from "@chestrapp/presets";
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { initializeAuth } from "firebase/auth";
-// import { initializeAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBgKQmVVt4OBkIILBN_MFIjFvFRBNwMquU",
-//     authDomain: "chestrapp.firebaseapp.com",
-//     databaseURL: "https://chestrapp-default-rtdb.firebaseio.com",
-//     projectId: "chestrapp",
-//     storageBucket: "chestrapp.appspot.com",
-//     messagingSenderId: "496271595233",
-//     appId: "1:496271595233:web:7b663daf53c9203b6909a0",
-//     measurementId: "G-VZ1J6363K0"
-// };
-
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const auth = initializeAuth(app);
-// const analytics = initializeAnalytics(app, {
-//     config: {
-//         send_page_view: false,
-//     },
-
-// });
+import { WEB_URL } from '../config'
 
 console.log('ServiceWorker script')
-
-// chrome.action.onClicked.addListener(
-//     (tab: chrome.tabs.Tab) => {
-//         chrome.scripting.executeScript({
-//             target: {
-//                 tabId: tab.id!
-//             },
-//             func: () => window.__CHESTR__
-//         },
-//             ([{ result }]: any) => {
-//                 console.log(result)
-//                 // chrome.action.setPopup({})
-//             }
-//         )
-//     }
-// )
 
 chrome.action.disable()
 
@@ -56,6 +10,7 @@ chrome.tabs.onUpdated.addListener(
     changeInfo: chrome.tabs.TabChangeInfo,
     tab: chrome.tabs.Tab,
   ) => {
+    console.log('chrome.tabs.onUpdated', changeInfo.status)
     if (changeInfo.status === 'complete') {
       chrome.scripting
         .executeScript({
@@ -65,7 +20,7 @@ chrome.tabs.onUpdated.addListener(
           files: ['preset.js'],
         })
         .then(([{ result }]: any) => {
-          if (result?.product?.url || result?.meta?.url) {
+          if (result?.product) {
             chrome.action.enable(tabId)
             chrome.action.setBadgeText({
               text: 'Y',
@@ -79,21 +34,13 @@ chrome.tabs.onUpdated.addListener(
             })
           }
         })
-        .catch(() => undefined)
+        .catch(console.error)
     }
-  },
-)
-
-chrome.runtime.onMessageExternal.addListener(
-  (request, sender, sendResponse) => {
-    console.log('request', request)
-
-    storage.local.set(request)
   },
 )
 
 chrome.runtime.onInstalled.addListener((details) => {
   if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
-    chrome.tabs.create({ url: 'https://chestr.com/sign-up' })
+    chrome.tabs.create({ url: WEB_URL + '/sign-up' })
   }
 })
